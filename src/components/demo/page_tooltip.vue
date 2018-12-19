2bb3536405736d8223daef831c39f52f8ef432a0
kongkongbuding <445276239@qq.com>
2018-12-19 19:20:33 +0800

    update

--- src/components/demo/page_tooltip.vue
+++ src/components/demo/page_tooltip.vue
@@ -22,9 +22,6 @@ export default {
       left: 100,
       top: 200
     }
-  },
-  mounted: function () {
-    console.log(this)
   }
 }
 </script>
@@ -32,10 +29,6 @@ export default {
 <style type="text/less" lang="less" scoped>
 .boxing {
   width: 300px; height: 240px; background: #cecece; border: #aaa;
-  .title {
-    height: 40px; background: #bfc981; width: 100%;
-    .closeBtn { height: 24px; width: 24px; float: right; margin: 8px; background: #d0d; cursor: pointer;}
-  }
   .boxBody {
     text-align: left;
     span { margin: 10px; }
